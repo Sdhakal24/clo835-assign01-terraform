bad4ac4b06b26d59988c83341c242011493f3e09
Sandesh Dhakal <sandesh841maple@gmail.com>
2025-01-31 14:54:44 -0500

    Update in output.tf

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -1,8 +1,11 @@
+
+
+# Output for public subnet ID 
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
 }
 
-
+# Output for VPC Id
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
