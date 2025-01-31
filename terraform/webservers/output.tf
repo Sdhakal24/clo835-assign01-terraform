14ccf306f9d4c576c25e84b23e0760eefd51d749
Sandesh Dhakal <sandesh841maple@gmail.com>
2025-01-31 15:08:50 -0500

    Update webservers/output.tf

--- terraform/webservers/output.tf
+++ terraform/webservers/output.tf
@@ -1,13 +1,15 @@
+# Output to display public Ip addres
 output "public_ip" {
   value = aws_instance.web_server.public_ip
 }
 
-# Output to show the ECR repository URL
+# Output to show the ECR repository URL for sql
 output "ecr_repository_url_sql" {
   value = aws_ecr_repository.sql.repository_url
   description = "The URL of the ECR repository"
 }
 
+# Output to show the ECR repository URL for flaskapp
 output "ecr_repository_url_flaskapp" {
   value = aws_ecr_repository.flaskapp.repository_url
   description = "The URL of the ECR repository"
