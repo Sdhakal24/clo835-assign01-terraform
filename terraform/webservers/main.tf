94eacbcb6b8a49e283e5f04e58bc45ffef8df9de
Sandesh Dhakal <sandesh841maple@gmail.com>
2025-02-02 18:05:08 -0500

    Update main.tf

--- terraform/webservers/main.tf
+++ terraform/webservers/main.tf
@@ -67,6 +67,14 @@ resource "aws_security_group" "web_server_sg" {
   }
 
   ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTP from everywhere for docker port"
     from_port   = 8081
     to_port     = 8083
