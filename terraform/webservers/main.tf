e3716613a345423b60d0d919ba5021a8d4df3032
Sandesh Dhakal <sandesh841maple@gmail.com>
2025-02-02 16:23:42 -0500

    Update main.tf

--- terraform/webservers/main.tf
+++ terraform/webservers/main.tf
@@ -66,6 +66,14 @@ resource "aws_security_group" "web_server_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP from everywhere for docker port"
+    from_port   = 8081
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
